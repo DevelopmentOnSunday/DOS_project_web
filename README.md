3690a5e044e715ef88a63f51f18b1f059cb524de
Jungminchae <minchae3618@gmail.com>
2021-06-28 18:56:56 +0900

    DOS web dev start

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DOS_project_web
 DOS Project 를 위한 웹개발
+
+### 개발중
+```bash
+# flask가 설치되어 있을 때
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path="/static")
+
+
+@app.route("/")
+def home_render():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="8000")
